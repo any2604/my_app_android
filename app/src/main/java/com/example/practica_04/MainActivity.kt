f74c25445adf1dd4b7621c82ea7074e8b70c17ac
any2604 <anybenga26@gmail.com>
2025-05-20 18:13:27 -0500

    Resuelto conflicto de fusión

--- app/src/main/java/com/example/practica_04/MainActivity.kt
+++ app/src/main/java/com/example/practica_04/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             Practica_04Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "STUDENTS",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
